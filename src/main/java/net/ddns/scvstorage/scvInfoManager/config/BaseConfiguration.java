e51e9cab1279e54cf131e46b5c4bf330eea67564
SangWon Jung <scvggggo@gmail.com>
2021-02-07 22:03:40 +0900

    remove unused function

--- src/main/java/net/ddns/scvstorage/scvInfoManager/config/BaseConfiguration.java
+++ src/main/java/net/ddns/scvstorage/scvInfoManager/config/BaseConfiguration.java
@@ -11,7 +11,7 @@ public class BaseConfiguration implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("home");
         registry.addViewController("/home").setViewName("home");
-        registry.addViewController("/hello").setViewName("hello");
+        //registry.addViewController("/hello").setViewName("hello");
     }
 
 }
