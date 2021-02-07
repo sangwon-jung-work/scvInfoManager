3db328bc1e0867ea46e77106d858333e1a700b04
SangWon Jung <scvggggo@gmail.com>
2021-02-07 19:33:30 +0900

    add spring profile print log

--- src/main/java/net/ddns/scvstorage/scvInfoManager/config/BaseConfiguration.java
+++ src/main/java/net/ddns/scvstorage/scvInfoManager/config/BaseConfiguration.java
@@ -1,19 +1,12 @@
 package net.ddns.scvstorage.scvInfoManager.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Profile("product")
 @Configuration
 public class BaseConfiguration implements WebMvcConfigurer {
-
-    @Bean
-    public String testConfigurations() {
-        return "LIVE";
-    }
 
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("home");
--- src/main/java/net/ddns/scvstorage/scvInfoManager/listener/StartedListener.java
+++ src/main/java/net/ddns/scvstorage/scvInfoManager/listener/StartedListener.java
@@ -1,5 +1,6 @@
 package net.ddns.scvstorage.scvInfoManager.listener;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
@@ -7,10 +8,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class StartedListener implements ApplicationListener<ApplicationStartedEvent> {
 
+    @Value("${spring.profiles.active}")
+    private String active;
+
     @Override
     public void onApplicationEvent(ApplicationStartedEvent arg0) {
         System.out.println("====================================");
-        System.out.println("Application is Started!");
+        System.out.println("Application is Started to " + active + "!");
         System.out.println("====================================");
     }
 
