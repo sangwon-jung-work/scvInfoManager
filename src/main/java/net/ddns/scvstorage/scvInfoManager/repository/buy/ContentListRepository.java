e44106f2cf0c01902a2b31b76b589c1777ec3338
SangWon Jung <scvggggo@gmail.com>
2021-02-07 22:05:17 +0900

    add search by id, test pagenation

--- src/main/java/net/ddns/scvstorage/scvInfoManager/repository/buy/ContentListRepository.java
+++ src/main/java/net/ddns/scvstorage/scvInfoManager/repository/buy/ContentListRepository.java
@@ -7,4 +7,10 @@ import net.ddns.scvstorage.scvInfoManager.entity.buy.ContentList;
 public interface ContentListRepository extends CrudRepository<ContentList, Integer> {
 
     Iterable<ContentList> findByContentTypeCd(String contentTypeCd);
+
+    Iterable<ContentList> findByContentTypeCdAndContentListId(String contentTypeCd, Integer contentListId);
+
+    //Page<ContentList> findAllPage(Pageable pageable);
+
+    //Page<ContentList> findByContentTypeCdPage(String contentTypeCd, Pageable pageable);
 }
