1d02f4bc4d9b4a3ffb51d8bf1a41ef680b6c5834
SangWon Jung <scvggggo@gmail.com>
2021-02-15 13:58:41 +0900

    구입정보 관련 화면 데이터 처리 추가(기존 등록자료 조회)

--- src/main/java/net/ddns/scvstorage/scvInfoManager/controller/WebController.java
+++ src/main/java/net/ddns/scvstorage/scvInfoManager/controller/WebController.java
@@ -1,5 +1,9 @@
 package net.ddns.scvstorage.scvInfoManager.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import net.ddns.scvstorage.scvInfoManager.common.util.DateUtil;
 import net.ddns.scvstorage.scvInfoManager.entity.buy.*;
 import net.ddns.scvstorage.scvInfoManager.entity.memo.*;
+import net.ddns.scvstorage.scvInfoManager.repository.buy.ContentListRepository;
+import net.ddns.scvstorage.scvInfoManager.repository.buy.DigitalContentListRepository;
+import net.ddns.scvstorage.scvInfoManager.repository.buy.ShippingInfoRepository;
+import net.ddns.scvstorage.scvInfoManager.repository.common.ComboData;
 
 @Controller
 @RequestMapping("/web")
@@ -30,6 +38,17 @@ public class WebController {
     final String LIST_STR = "list";
     final String INSERT_STR = "insert";
 
+    @Autowired
+    private ContentListRepository contentListRepository;
+
+    @Autowired
+    private DigitalContentListRepository digitalContentListRepository;
+
+    @Autowired
+    private ShippingInfoRepository shippingInfoRepository;
+
+    // 공통 YN 콤보 데이터
+    List<String> commonYn = Arrays.asList("N","Y");
 
     // 상품구입정보
     @GetMapping(BUY_PATH_U + CON1_STR + LIST_STR)
@@ -41,12 +60,44 @@ public class WebController {
     @GetMapping(BUY_PATH_U + CON1_STR + INSERT_STR)
     public String insertContent(Model model) {
 
+        // 공통으로 사용할 콤보용 YN value
+        model.addAttribute("ynComboValue", commonYn);
+
+        // 장르를 조회해서 콤보로 만든다
+        List<ComboData> allGenre = contentListRepository.getGenre();
+        model.addAttribute("allGenre", allGenre);
+
+        // 상품발매국가를 조회해서 콤보로 만든다
+        List<ComboData> allSalesCountry = contentListRepository.getSalesCountry();
+        model.addAttribute("allSalesCountry", allSalesCountry);
+
+        // 상품종류를 조회해서 콤보로 만든다
+        List<ComboData> allConditionType = contentListRepository.getConditionType();
+        model.addAttribute("allConditionType", allConditionType);
+
+        // 상품가격통화를 조회해서 콤보로 만든다
+        List<ComboData> allCurrency = contentListRepository.getCurrency();
+        model.addAttribute("allCurrency", allCurrency);
+
+        // 상품구입처를 조회해서 콤보로 만든다
+        List<ComboData> allBuyingLocation = contentListRepository.getBuyingLocation();
+        model.addAttribute("allBuyingLocation", allBuyingLocation);
+
+        // 결제수단을 조회해서 콤보로 만든다
+        List<ComboData> allPaymentMethod = contentListRepository.getPaymentMethod();
+        model.addAttribute("allPaymentMethod", allPaymentMethod);
+
+        // 주문번호가 있는 배송정보를 조회해서 콤보로 만든다
+        List<ComboData> allShippingInfo = shippingInfoRepository.getOrderNumOrderByPK();
+        model.addAttribute("allshippingInfo", allShippingInfo);
+
         ContentList contentList = new ContentList();
 
-        contentList.setXrateYn("N");
-        contentList.setWriteReviewYn("N");
-        contentList.setReViewYn("N");
+        contentList.setXrateYn("N"); // 성인구분
+        contentList.setWriteReviewYn("N"); // 감상정리여부
+        contentList.setReViewYn("N"); // 재감상여부
 
+        // 화면 초기 데이터
         model.addAttribute("ContentList", contentList);
 
         return BUY_PATH_F + CON1_STR + INSERT_STR;
@@ -62,10 +113,28 @@ public class WebController {
     @GetMapping(BUY_PATH_U + CON2_STR + INSERT_STR)
     public String insertDigitalContent(Model model) {
 
+        // 포멧을 조회해서 콤보로 만든다
+        List<ComboData> allFormat = digitalContentListRepository.getFormat();
+        model.addAttribute("allFormat", allFormat);
+
+        // 통화를 조회해서 콤보로 만든다
+        List<ComboData> allCurrency = digitalContentListRepository.getCurrency();
+        model.addAttribute("allCurrency", allCurrency);
+
+        // 구입처를 조회해서 콤보로 만든다
+        List<ComboData> allBuyingLocation = digitalContentListRepository.getBuyingLocation();
+        model.addAttribute("allBuyingLocation", allBuyingLocation);
+
+        // 레이블을 조회해서 콤보로 만든다
+        List<ComboData> allReleaseLabel = digitalContentListRepository.getReleaseLabel();
+        model.addAttribute("allReleaseLabel", allReleaseLabel);
+
         DigitalContentList digitalContentList = new DigitalContentList();
 
+        // 구입일시
         digitalContentList.setBuyingDate( DateUtil.stringToDate( DateUtil.getCurrentDate("yyyy-MM-dd HH:mm"), "yyyy-MM-dd HH:mm" ) );
 
+        // 화면 초기 데이터
         model.addAttribute("DigitalContentList", digitalContentList);
 
         return BUY_PATH_F + CON2_STR + INSERT_STR;
@@ -81,6 +150,19 @@ public class WebController {
     @GetMapping(BUY_PATH_U + CON3_STR + INSERT_STR)
     public String insertShippingInfo(Model model) {
 
+        // 배송가격통화를 조회해서 콤보로 만든다
+        List<ComboData> allCurrency = shippingInfoRepository.getCurrency();
+        model.addAttribute("allCurrency", allCurrency);
+
+        // 배송처를 조회해서 콤보로 만든다
+        List<ComboData> allBuyingLocation = shippingInfoRepository.getBuyingLocation();
+        model.addAttribute("allBuyingLocation", allBuyingLocation);
+
+        // 결제수단을 조회해서 콤보로 만든다
+        List<ComboData> allPaymentMethod = shippingInfoRepository.getPaymentMethod();
+        model.addAttribute("allPaymentMethod", allPaymentMethod);
+
+        // 화면 초기 데이터
         model.addAttribute("ShippingInfo", new ShippingInfo());
 
         return BUY_PATH_F + CON3_STR + INSERT_STR;
