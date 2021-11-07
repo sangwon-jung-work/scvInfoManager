8bcd4b51024c0c137b1d04371861f9fce9045b83
SangWon Jung <sangwon.jung.work@gmail.com>
2021-11-07 21:39:27 +0900

    add put, delete method

--- src/main/java/net/ddns/scvstorage/scvInfoManager/controller/BuyController.java
+++ src/main/java/net/ddns/scvstorage/scvInfoManager/controller/BuyController.java
@@ -1,7 +1,9 @@
 package net.ddns.scvstorage.scvInfoManager.controller;
 
 import java.util.Arrays;
+import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.web.bind.WebDataBinder;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -66,7 +70,7 @@ class BuyController {
     public Iterable<ContentList> getContentList(@PathVariable String contentTypeCd, @PathVariable Integer contentId) {
 
         Integer ids[] = {contentId};
-        Iterable<Integer> idList = Arrays. asList(ids);
+        Iterable<Integer> idList = Arrays.asList(ids);
 
         if( contentTypeCd.toUpperCase().equals("ALL") ) {
             return contentListRepository.findAllById(idList);
@@ -97,42 +101,38 @@ class BuyController {
      * @return
      */
     @PostMapping("/content")
-    ContentList postContentList(@ModelAttribute ContentList contentList) {
-        return contentListRepository.save(contentList);
+    Iterable<ContentList> postContentList(@ModelAttribute Iterable<ContentList> contentList) {
+        return contentListRepository.saveAll(contentList);
     }
 
     /**
-     * 구입이력 수정(1건)
+     * 구입이력 수정(ID 1건)
      * @param contentList 수정할 구입이력 데이터
      * @param contentId 구입이력ID
      * @return
-     
+     */
     @PutMapping("/contentList/{contentId}")
-    ContentList putContentList(@RequestBody List<ContentList> contentList, @PathVariable Integer contentId) {
+    ContentList putContent(@RequestBody ContentList contentList, @PathVariable Integer contentId) {
         
-        System.out.println(contentList.size());
-
         return contentListRepository.findById(contentId)
-            .map(contentlist -> {
-                BeanUtils.copyProperties(contentList, contentlist);
-                contentlist.setContentListId(contentId);
-                return contentlist;
-                //return contentListRepository.save(contentlist);
-            })
-            .orElseGet(() -> {
-                contentList.setContentListId(contentId);
-                return contentList;
-                //return contentListRepository.save(contentList);
+            .map(selContentList -> {
+                BeanUtils.copyProperties(contentList, selContentList);
+                selContentList.setContentListId(contentId);
+                return contentListRepository.save(selContentList);
+            
+            }).orElseGet( () -> {
+                
+                return contentListRepository.save(contentList);
             });
-    }
-    */
 
+    }
+    
     /**
-     * 구입이력 삭제
+     * 구입이력 삭제(ID 1건)
      * @param contentId 삭제할 구입이력ID
      */
     @DeleteMapping("/content/{contentId}")
-    void deleteContentList(@PathVariable Integer contentId) {
+    void deleteContent(@PathVariable Integer contentId) {
         contentListRepository.deleteById(contentId);
     }
 
@@ -151,27 +151,87 @@ class BuyController {
      * @return
      */
     @PostMapping("/dcontent")
-    DigitalContentList postDigitalContentList(@ModelAttribute DigitalContentList digitalContentList) {
-        return digitalContentListRepository.save(digitalContentList);
+    Iterable<DigitalContentList> postDigitalContentList(@ModelAttribute Iterable<DigitalContentList> digitalContentList) {
+        return digitalContentListRepository.saveAll(digitalContentList);
     }
 
     /**
-     * 
+     * 구입이력(디지털) 수정(ID 1건)
+     * @param dcontentId 구입이력ID
      * @return
      */
+    @PutMapping("/dcontent/{dcontentId}")
+    DigitalContentList putDigitalContent(@RequestBody DigitalContentList digitalContentList, @PathVariable Integer dcontentId) {
+        
+        return digitalContentListRepository.findById(dcontentId)
+            .map(selDigitalContentList -> {
+                BeanUtils.copyProperties(digitalContentList, selDigitalContentList);
+                selDigitalContentList.setDigitalContentListId(dcontentId);
+                return digitalContentListRepository.save(selDigitalContentList);
+            
+            }).orElseGet( () -> {
+                
+                return digitalContentListRepository.save(digitalContentList);
+            });
+
+    }
+    
+    /**
+     * 구입이력(디지털) 삭제(ID 1건)
+     * @param dcontentId 삭제할 구입이력ID
+     */
+    @DeleteMapping("/dcontent/{dcontentId}")
+    void deleteDigitalContent(@PathVariable Integer dcontentId) {
+        digitalContentListRepository.deleteById(dcontentId);
+    }
 
 
-    // 데이터 조회
-    @GetMapping("/shippinginfo")
-    public Iterable<ShippingInfo> selectShippingInfoList() {
+    /**
+     * 배송정보 조회
+     * @return
+     */
+    @GetMapping("/shipping")
+    public Iterable<ShippingInfo> getShippingInfoList() {
         return shippingInfoRepository.findAll();
     }
 
-    @PostMapping("/shippinginfo")
-    ShippingInfo addShippingInfo(@ModelAttribute ShippingInfo shippingInfo) {
-        
-        //System.out.println(shippingInfo.toString());
-        
-        return shippingInfoRepository.save(shippingInfo);
+    /** 배송정보 등록
+     * @param shippingInfo 배송정보 데이터
+     * @return
+     */
+    @PostMapping("/shipping")
+    Iterable<ShippingInfo> postShippingInfoList(@ModelAttribute Iterable<ShippingInfo> shippingInfo) {
+        return shippingInfoRepository.saveAll(shippingInfo);
     }
+
+    /**
+     * 배송정보 수정(ID 1건)
+     * @param shippingId 배송정보ID
+     * @return
+     */
+    @PutMapping("/shipping/{shippingId}")
+    ShippingInfo putShippingInfo(@RequestBody ShippingInfo shippingInfo, @PathVariable Integer shippingId) {
+        
+        return shippingInfoRepository.findById(shippingId)
+            .map(selShippingInfoList -> {
+                BeanUtils.copyProperties(shippingInfo, selShippingInfoList);
+                selShippingInfoList.setShippingInfoId(shippingId);
+                return shippingInfoRepository.save(selShippingInfoList);
+            
+            }).orElseGet( () -> {
+                
+                return shippingInfoRepository.save(shippingInfo);
+            });
+
+    }
+    
+    /**
+     * 배송정보 삭제(ID 1건)
+     * @param dcontentId 삭제할 배송정보ID
+     */
+    @DeleteMapping("/shipping/{shippingId}")
+    void deleteShippingInfo(@PathVariable Integer shippingId) {
+        shippingInfoRepository.deleteById(shippingId);
+    }
+
 }
--- src/main/java/net/ddns/scvstorage/scvInfoManager/controller/MemoController.java
+++ src/main/java/net/ddns/scvstorage/scvInfoManager/controller/MemoController.java
@@ -1,12 +1,17 @@
 package net.ddns.scvstorage.scvInfoManager.controller;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,41 +35,162 @@ public class MemoController {
     @Autowired
     private MemoTimeRepository memoTimeRepository;
 
-    
     @InitBinder
     public void initBinder(WebDataBinder binder) {
         binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
     }
 
+
+    /**
+     * 공통코드관리 조회
+     * @return
+     */
     @GetMapping("/comcd")
-    public Iterable<CommonCdList> selectCommonCdList() {
+    public Iterable<CommonCdList> getCommonCdList() {
         return commonCdListRepository.findAll();
     }
 
+    /**
+     * 공통코드관리 등록
+     * @param commonCdList 공통코드관리 데이터
+     * @return
+     */
     @PostMapping("/comcd")
-    CommonCdList addCommonCd(@ModelAttribute CommonCdList commonCdList) {
-        return commonCdListRepository.save(commonCdList);
+    Iterable<CommonCdList> postCommonCd(@ModelAttribute Iterable<CommonCdList> commonCdList) {
+        return commonCdListRepository.saveAll(commonCdList);
+    }
+
+    /**
+     * 공통코드관리 수정(ID 1건)
+     * @param contentList 수정할 공통코드관리 데이터
+     * @param commonCdId 공통코드관리ID
+     * @return
+     */
+    @PutMapping("/comcd/{commonCdId}")
+    CommonCdList putCommonCd(@RequestBody CommonCdList commonCdList, @PathVariable Integer commonCdId) {
+        
+        return commonCdListRepository.findById(commonCdId)
+            .map(selCommonCdList -> {
+                BeanUtils.copyProperties(commonCdList, selCommonCdList);
+                selCommonCdList.setCommonCdListId(commonCdId);
+                return commonCdListRepository.save(selCommonCdList);
+            
+            }).orElseGet( () -> {
+                
+                return commonCdListRepository.save(commonCdList);
+            });
+
+    }
+    
+    /**
+     * 공통코드관리 삭제(ID 1건)
+     * @param commonCdId 삭제할 공통코드관리ID
+     */
+    @DeleteMapping("/comcd/{commonCdId}")
+    void deleteCommonCd(@PathVariable Integer commonCdId) {
+        commonCdListRepository.deleteById(commonCdId);
     }
 
 
-    @GetMapping("/locationcd")
-    public Iterable<LocationCdList> selectLocationCdList() {
+    /**
+     * 방위치관리 조회
+     * @return
+     */
+    @GetMapping("/location")
+    public Iterable<LocationCdList> getLocationCdList() {
         return locationCdListRepository.findAll();
     }
 
-    @PostMapping("/locationcd")
-    LocationCdList addLocationCdList(@ModelAttribute LocationCdList locationCdList) {
-        return locationCdListRepository.save(locationCdList);
+    /**
+     * 방위치관리 등록
+     * @param locationCdList 방위치관리 데이터
+     * @return
+     */
+    @PostMapping("/location")
+    Iterable<LocationCdList> postLocationCdList(@ModelAttribute Iterable<LocationCdList> locationCdList) {
+        return locationCdListRepository.saveAll(locationCdList);
+    }
+
+    /**
+     * 방위치관리 수정(ID 1건)
+     * @param locationCdList 수정할 방위치관리 데이터
+     * @param locationId 방위치관리ID
+     * @return
+     */
+    @PutMapping("/location/{locationId}")
+    LocationCdList putLocationCdList(@RequestBody LocationCdList locationCdList, @PathVariable Integer locationId) {
+        
+        return locationCdListRepository.findById(locationId)
+            .map(selLocationCdList -> {
+                BeanUtils.copyProperties(locationCdList, selLocationCdList);
+                selLocationCdList.setLocationCdListId(locationId);
+                return locationCdListRepository.save(selLocationCdList);
+            
+            }).orElseGet( () -> {
+                
+                return locationCdListRepository.save(locationCdList);
+            });
+
+    }
+    
+    /**
+     * 방위치관리 삭제(ID 1건)
+     * @param locationId 삭제할 방위치관리ID
+     */
+    @DeleteMapping("/location/{locationId}")
+    void deleteLocationCdList(@PathVariable Integer locationId) {
+        locationCdListRepository.deleteById(locationId);
     }
 
 
+    /**
+     * 메모기록 조회
+     * @return
+     */
     @GetMapping("/memotime")
-    public Iterable<MemoTime> selectMemoTime() {
+    public Iterable<MemoTime> getMemoTime() {
         return memoTimeRepository.findAll();
     }
 
+    /**
+     * 메모기록 등록
+     * @param memoTime 방위치관리 데이터
+     * @return
+     */
     @PostMapping("/memotime")
-    MemoTime addMemoTime(@ModelAttribute MemoTime memoTime) {
-        return memoTimeRepository.save(memoTime);
+    Iterable<MemoTime> postMemoTime(@ModelAttribute Iterable<MemoTime> memoTime) {
+        return memoTimeRepository.saveAll(memoTime);
     }
+
+    /**
+     * 메모기록 수정(ID 1건)
+     * @param memoTime 수정할 메모기록 데이터
+     * @param memotimeId 메모기록ID
+     * @return
+     */
+    @PutMapping("/memotime/{memotimeId}")
+    MemoTime putMemoTime(@RequestBody MemoTime memoTime, @PathVariable Integer memotimeId) {
+        
+        return memoTimeRepository.findById(memotimeId)
+            .map(selMemotime -> {
+                BeanUtils.copyProperties(memoTime, selMemotime);
+                selMemotime.setMemoTimeId(memotimeId);
+                return memoTimeRepository.save(selMemotime);
+            
+            }).orElseGet( () -> {
+                
+                return memoTimeRepository.save(memoTime);
+            });
+
+    }
+    
+    /**
+     * 메모기록 삭제(ID 1건)
+     * @param locationId 삭제할 메모기록ID
+     */
+    @DeleteMapping("/memotime/{memotimeId}")
+    void deleteMemoTime(@PathVariable Integer memotimeId) {
+        memoTimeRepository.deleteById(memotimeId);
+    }
+
 }
