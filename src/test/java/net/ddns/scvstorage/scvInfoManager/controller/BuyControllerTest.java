8d8ec5454cbd94c448906cfbd8c2411ab5082653
SangWon Jung <sangwon.jung.work@gmail.com>
2022-05-05 21:24:32 +0900

    modify test method from RestTemplate to MockMvc add test case for post request

--- src/test/java/net/ddns/scvstorage/scvInfoManager/controller/BuyControllerTest.java
+++ src/test/java/net/ddns/scvstorage/scvInfoManager/controller/BuyControllerTest.java
@@ -1,121 +1,264 @@
 package net.ddns.scvstorage.scvInfoManager.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-import java.io.IOException;
+import javax.transaction.Transactional;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import net.ddns.scvstorage.scvInfoManager.ScvInfoManagerApplication;
 import net.ddns.scvstorage.scvInfoManager.entity.buy.ContentList;
+import net.ddns.scvstorage.scvInfoManager.entity.buy.DigitalContentList;
+import net.ddns.scvstorage.scvInfoManager.entity.buy.ShippingInfo;
+import net.ddns.scvstorage.scvInfoManager.entity.buy.ContentList.contentType;
+
+import static org.hamcrest.Matchers.containsString;
 
 @SpringBootTest(classes = ScvInfoManagerApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@Transactional
 @TestMethodOrder(OrderAnnotation.class)
 public class BuyControllerTest {
     
-    @LocalServerPort
-	private int port;
+    @Autowired
+    MockMvc mockMvc;
 
-    TestRestTemplate restTemplate = new TestRestTemplate();
-
-	HttpHeaders headers = new HttpHeaders();
-
-    Logger logger = LoggerFactory.getLogger(BuyControllerTest.class);
-    
     /**
      * 구입이력 조회(전체) 테스트
+     * @throws Exception
      */
     @Test
     @Order(1)
-    @DisplayName("ContentList findAll StatusCode is OK")
-    public void getContentList_findAll_getStatusCodeOK() throws IOException, InterruptedException {
+    @DisplayName("ContentList findAll, StatusCode is OK")
+    public void getContentListByTypeCd_findAll_getStatusCodeOK() throws Exception {
         
-        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
-        
-        ResponseEntity<String> response = restTemplate.exchange(
-                createURLWithPort("/buy/content/all"),
-                HttpMethod.GET, entity, String.class);
-        
-        String resBody = response.getBody();
-        logger.info("getContentList_findAll_getStatusCodeOK() body length {}", resBody.length());
-        assertNotNull(resBody);
-        assertEquals(response.getStatusCode().value(), HttpStatus.OK.value());
+        mockMvc.perform(
+            MockMvcRequestBuilders.get("/buy/content/all")
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString( ContentList.contentType.Album.name() )));
     }
 
     /**
      * 구입이력 조회(구입구분코드별) 테스트
+     * @throws Exception
      */
     @Test
     @Order(2)
-    @DisplayName("ContentList find by contentType StatusCode is OK")
-    public void getContentList_find_Bluray_getStatusCodeOK() throws IOException, InterruptedException {
+    @DisplayName("ContentList find by contentType, StatusCode is OK")
+    public void getContentListByTypeCd_find_Bluray_getStatusCodeOK() throws Exception {
         
-        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+        mockMvc.perform(
+            MockMvcRequestBuilders.get("/buy/content/".concat(ContentList.contentType.Bluray.name()))
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString( ContentList.contentType.Bluray.name() )));
+    }
+
+    /**
+     * 구입이력 조회(ID) 테스트
+     * @throws Exception
+     */
+    @Test
+    @Order(3)
+    @DisplayName("ContentList find by contentType and ID, StatusCode is OK")
+    public void getContentListById_findBy_contentTypeAndID_getStatusCodeOK() throws Exception {
         
-        ResponseEntity<String> response = restTemplate.exchange(
-                createURLWithPort("/buy/content/".concat(ContentList.contentType.Bluray.name())),
-                HttpMethod.GET, entity, String.class);
-                
-        String resBody = response.getBody();
-        logger.info("getContentList_find_Bluray_getStatusCodeOK() body length {}", resBody.length());
-        assertNotNull(resBody);
-        assertEquals(response.getStatusCode().value(), HttpStatus.OK.value());
+        mockMvc.perform(
+            MockMvcRequestBuilders.get("/buy/content/".concat(ContentList.contentType.Album.name()).concat("/1430") )
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString( "contentListId" )));
+    }
+
+    /**
+     * 구입이력 입력 테스트
+     * @throws Exception
+     */
+    @Test
+    @Order(4)
+    @DisplayName("ContentList create, StatusCode is OK")
+    public void postContentList_create_getStatusCodeOK() throws Exception {
+        
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd" );
+        
+        final ContentList testData = new ContentList();
+        testData.setContentTypeCd( contentType.Bluray );
+        testData.setTitle( "극장판 테스트를 위하여" );
+        testData.setVersion( "넘버링한정생산판" );
+        testData.setReceiptDate( new Date() );
+        testData.setSalesCountry( "KOR" );
+        testData.setConditionType( "신품" );
+        testData.setBuyingLocation( "알라딘" );
+        testData.setPaymentMethod( "VISA1123" );
+        testData.setReleaseDate( new Date() );
+        testData.setMemo( "테스트데이터" );
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/buy/content")
+                .param("contentTypeCd", testData.getContentTypeCd().name())
+                .param("title", testData.getTitle())
+                .param("version", testData.getVersion())
+                .param("receiptDate", format.format( testData.getReceiptDate() ) )
+                .param("salesCountry", testData.getSalesCountry())
+                .param("conditionType", testData.getConditionType())
+                .param("buyingLocation", testData.getBuyingLocation())
+                .param("paymentMethod", testData.getPaymentMethod())
+                .param("releaseDate", format.format( testData.getReceiptDate() ) )
+                .param("memo", testData.getMemo())
+                .characterEncoding("UTF-8"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString( "contentListId" )))
+                .andExpect(content().string(containsString( testData.getTitle() )));
     }
 
     /**
      * 구입이력(디지털) 조회 테스트
      * @return
+     * @throws Exception
      */
     @Test
-    @Order(3)
+    @Order(7)
     @DisplayName("DigitalContentList findAll StatusCode is OK")
-    public void getDigitalContentList_findAll_getStatusCodeOK() {
-        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+    public void getDigitalContentList_findAll_getStatusCodeOK() throws Exception {
         
-        ResponseEntity<String> response = restTemplate.exchange(
-                createURLWithPort("/buy/dcontent"),
-                HttpMethod.GET, entity, String.class);
-                
-        String resBody = response.getBody();
-        logger.info("getDigitalContentList_findAll_getStatusCodeOK() body length {}", resBody.length());
-        assertNotNull(resBody);
-        assertEquals(response.getStatusCode().value(), HttpStatus.OK.value());   
+        mockMvc.perform(
+            MockMvcRequestBuilders.get("/buy/dcontent")
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString( "digitalContentListId" )));
+        
+    }
+
+    /**
+     * 구입이력(디지털) 조회(ID) 테스트
+     * @throws Exception
+     */
+    @Test
+    @Order(8)
+    @DisplayName("DigitalContentList findById, StatusCode is OK")
+    public void getDigitalContentListById_findById_getStatusCodeOK() throws Exception {
+        
+        mockMvc.perform(
+            MockMvcRequestBuilders.get("/buy/dcontent".concat("/2") )
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString( "digitalContentListId" )));
+    }
+
+    /**
+     * 구입이력 입력 테스트
+     * @throws Exception
+     */
+    @Test
+    @Order(9)
+    @DisplayName("DigitalContentList create, StatusCode is OK")
+    public void postDigitalContentList_create_getStatusCodeOK() throws Exception {
+        
+        DateFormat isoFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm" );
+        DateFormat simpleFormat = new SimpleDateFormat("yyyy-MM-dd" );
+        
+        final DigitalContentList testData = new DigitalContentList();
+        testData.setTitle( "테스트를 위하여" );
+        testData.setArtist( "테스트 가수" );
+        testData.setAlbum( "Test Album Name" );
+        testData.setFormat("FLAC");
+        testData.setSampleRate(48000);
+        testData.setBitDepth(24);
+        testData.setSize( Float.valueOf("130.56") );
+        testData.setBuyingDate( new Date() );
+        testData.setBuyingLocation("벅스");
+        testData.setReleaseLabel("KakaoM");
+        testData.setReleaseDate( new Date() );
+        testData.setMemo( "테스트데이터" );
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/buy/dcontent")
+                .param("title", testData.getTitle())
+                .param("artist", testData.getArtist())
+                .param("album", testData.getAlbum())
+                .param("format", testData.getFormat() )
+                .param("sampleRate", String.valueOf( testData.getSampleRate() ) )
+                .param("bitDepth", String.valueOf( testData.getBitDepth() ) )
+                .param("size", String.valueOf( testData.getSize() ) )
+                .param("buyingDate", isoFormat.format( testData.getBuyingDate() ) )
+                .param("buyingLocation", testData.getBuyingLocation() )
+                .param("releaseLabel", testData.getReleaseLabel() )
+                .param("releaseDate", simpleFormat.format( testData.getReleaseDate() ) )
+                .param("memo", testData.getMemo())
+                .characterEncoding("UTF-8"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString( "digitalContentListId" )))
+                .andExpect(content().string(containsString( testData.getTitle() )));
     }
 
     /**
      * 배송정보 조회 테스트
      */
     @Test
-    @Order(4)
+    @Order(12)
     @DisplayName("ShippingInfoList findAll StatusCode is OK")
-    public void getShippingInfoList_findAll_getStatusCodeOK() {
-        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+    public void getShippingInfoList_findAll_getStatusCodeOK() throws Exception {
         
-        ResponseEntity<String> response = restTemplate.exchange(
-                createURLWithPort("/buy/shipping"),
-                HttpMethod.GET, entity, String.class);
-                
-        String resBody = response.getBody();
-        logger.info("getShippingInfoList_findAll_getStatusCodeOK() body length {}", resBody.length());
-        assertNotNull(resBody);
-        assertEquals(response.getStatusCode().value(), HttpStatus.OK.value());        
+        mockMvc.perform(
+            MockMvcRequestBuilders.get("/buy/shipping")
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString( "shippingInfoId" )));
+        
     }
 
-    private String createURLWithPort(String uri) {
-		return "http://localhost:" + port + uri;
-	}
+    /**
+     * 배송정보 조회(ID) 테스트
+     * @throws Exception
+     */
+    @Test
+    @Order(13)
+    @DisplayName("ShippingInfoList findById, StatusCode is OK")
+    public void getShippingInfoListById_findById_getStatusCodeOK() throws Exception {
+        
+        mockMvc.perform(
+            MockMvcRequestBuilders.get("/buy/shipping".concat("/1083") )
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString( "shippingInfoId" )));
+    }
+
+    /**
+     * 구입이력 입력 테스트
+     * @throws Exception
+     */
+    @Test
+    @Order(14)
+    @DisplayName("ShippingInfoList create, StatusCode is OK")
+    public void postShippingInfoList_create_getStatusCodeOK() throws Exception {
+        
+        final ShippingInfo testData = new ShippingInfo();
+        testData.setBuyingLocation("이하넥스");
+        testData.setPaymentMethod("AMEX4457");
+        testData.setMemo( "테스트데이터" );
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/buy/shipping")
+                .param("buyingLocation", testData.getBuyingLocation())
+                .param("paymentMethod", testData.getPaymentMethod())
+                .param("memo", testData.getMemo())
+                .characterEncoding("UTF-8"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString( "shippingInfoId" )))
+                .andExpect(content().string(containsString( testData.getBuyingLocation() )));
+    }
 }
--- src/test/java/net/ddns/scvstorage/scvInfoManager/controller/MemoControllerTest.java
+++ src/test/java/net/ddns/scvstorage/scvInfoManager/controller/MemoControllerTest.java
@@ -1,84 +1,211 @@
 package net.ddns.scvstorage.scvInfoManager.controller;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import javax.transaction.Transactional;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import net.ddns.scvstorage.scvInfoManager.ScvInfoManagerApplication;
+import net.ddns.scvstorage.scvInfoManager.entity.memo.CommonCdList;
+import net.ddns.scvstorage.scvInfoManager.entity.memo.LocationCdList;
+import net.ddns.scvstorage.scvInfoManager.entity.memo.MemoTime;
+
+import static org.hamcrest.Matchers.containsString;
 
 @SpringBootTest(classes = ScvInfoManagerApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@Transactional
 @TestMethodOrder(OrderAnnotation.class)
 public class MemoControllerTest {
     
-    @LocalServerPort
-	private int port;
-
-    TestRestTemplate restTemplate = new TestRestTemplate();
-
-	HttpHeaders headers = new HttpHeaders();
+    @Autowired
+    MockMvc mockMvc;
     
     /**
      * 공통코드관리 조회 테스트
+     * @throws Exception
      */
     @Test
     @Order(1)
     @DisplayName("CommonCdList findAll StatusCode is OK")
-    void getCommonCdList_findAll_getStatusCodeOK() {
-        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+    public void getCommonCdList_findAll_getStatusCodeOK() throws Exception {
         
-        ResponseEntity<String> response = restTemplate.exchange(
-                createURLWithPort("/memo/comcd"),
-                HttpMethod.GET, entity, String.class);
-                
-        assertEquals(response.getStatusCode().value(), HttpStatus.OK.value());
+        mockMvc.perform(
+            MockMvcRequestBuilders.get("/memo/comcd")
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString( "commonCdListId" )));
+    }
+
+    /**
+     * 공통코드관리 조회(ID) 테스트
+     * @throws Exception
+     */
+    @Test
+    @Order(2)
+    @DisplayName("CommonCdList findById, StatusCode is OK")
+    public void getCommonCdListById_findById_getStatusCodeOK() throws Exception {
+        
+        mockMvc.perform(
+            MockMvcRequestBuilders.get("/memo/comcd".concat("/21") )
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString( "commonCdListId" )));
+    }
+
+    /**
+     * 공통코드관리 입력 테스트
+     * @throws Exception
+     */
+    @Test
+    @Order(4)
+    @DisplayName("CommonCdList create, StatusCode is OK")
+    public void postCommonCd_create_getStatusCodeOK() throws Exception {
+        
+        final CommonCdList testData = new CommonCdList();
+        testData.setCdKind("MEMO_CD");
+        testData.setCd("010");
+        testData.setCdNm("테스트성공");
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/memo/comcd")
+                .param("cdKind", testData.getCdKind())
+                .param("cd", testData.getCd())
+                .param("cdNm", testData.getCdNm())
+                .characterEncoding("UTF-8"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString( "commonCdListId" )))
+                .andExpect(content().string(containsString( testData.getCdNm() )));
     }
 
     /**
      * 방위치관리 조회 테스트
+     * @throws Exception
      */
     @Test
-    @Order(2)
+    @Order(6)
     @DisplayName("LocationCdList findAll StatusCode is OK")
-    void getLocationCdList_findAll_getStatusCodeOK() {
-        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+    public void getLocationCdList_findAll_getStatusCodeOK() throws Exception {
         
-        ResponseEntity<String> response = restTemplate.exchange(
-                createURLWithPort("/memo/location"),
-                HttpMethod.GET, entity, String.class);
-                
-        assertEquals(response.getStatusCode().value(), HttpStatus.OK.value());
+        mockMvc.perform(
+            MockMvcRequestBuilders.get("/memo/location")
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString( "locationCdListId" )));
+    }
+
+    /**
+     * 방위치관리 조회(ID) 테스트
+     * @throws Exception
+     */
+    @Test
+    @Order(7)
+    @DisplayName("LocationCdList findById, StatusCode is OK")
+    public void getLocationCdListById_findById_getStatusCodeOK() throws Exception {
+        
+        mockMvc.perform(
+            MockMvcRequestBuilders.get("/memo/location".concat("/1982") )
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString( "locationCdListId" )));
+    }
+
+    /**
+     * 방위치관리 입력 테스트
+     * @throws Exception
+     */
+    @Test
+    @Order(8)
+    @DisplayName("LocationCdList create, StatusCode is OK")
+    public void postLocationCdList_create_getStatusCodeOK() throws Exception {
+        
+        final LocationCdList testData = new LocationCdList();
+        testData.setLocationCd("099");
+        testData.setLocationDesc("서울특별시 관악구 관악산길 354 203호");
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/memo/location")
+                .param("locationCd", testData.getLocationCd())
+                .param("locationDesc", testData.getLocationDesc())
+                .characterEncoding("UTF-8"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString( "locationCdListId" )))
+                .andExpect(content().string(containsString( testData.getLocationDesc() )));
     }
 
     /**
      * 메모기록 조회 테스트
+     * @throws Exception
      */
     @Test
-    @Order(3)
+    @Order(11)
     @DisplayName("MemoTime findAll StatusCode is OK")
-    void getMemoTime_findAll_getStatusCodeOK() {
-        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+    public void getMemoTime_findAll_getStatusCodeOK() throws Exception {
         
-        ResponseEntity<String> response = restTemplate.exchange(
-                createURLWithPort("/memo/memotime"),
-                HttpMethod.GET, entity, String.class);
-                
-        assertEquals(response.getStatusCode().value(), HttpStatus.OK.value());
+        mockMvc.perform(
+            MockMvcRequestBuilders.get("/memo/memotime")
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString( "memoTimeId" )));
     }
 
-    private String createURLWithPort(String uri) {
-		return "http://localhost:" + port + uri;
-	}
+    /**
+     * 메모기록 조회(ID) 테스트
+     * @throws Exception
+     */
+    @Test
+    @Order(12)
+    @DisplayName("MemoTime findById, StatusCode is OK")
+    public void getMemoTimeById_findById_getStatusCodeOK() throws Exception {
+        
+        mockMvc.perform(
+            MockMvcRequestBuilders.get("/memo/memotime".concat("/20") )
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString( "memoTimeId" )));
+    }
+
+    /**
+     * 메모기록 입력 테스트
+     * @throws Exception
+     */
+    @Test
+    @Order(13)
+    @DisplayName("MemoTime create, StatusCode is OK")
+    public void postMemoTime_create_getStatusCodeOK() throws Exception {
+        
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd" );
+        
+        final MemoTime testData = new MemoTime();
+        testData.setRoomLocationCd("099");
+        testData.setMemoTypeCd("010");
+        testData.setMemoDate( new Date() );
+        testData.setNote("테스트입력 데이터");
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/memo/memotime")
+                .param("roomLocationCd", testData.getRoomLocationCd())
+                .param("memoTypeCd", testData.getMemoTypeCd())
+                .param("memoDate", format.format( testData.getMemoDate() ) )
+                .param("note", testData.getNote())
+                .characterEncoding("UTF-8"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString( "memoTimeId" )))
+                .andExpect(content().string(containsString( testData.getNote() )));
+    }
 
 }
